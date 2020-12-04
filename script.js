b0b9f005f40c97a02e411302ccd0e25aa2160307
invalid2 <75389191+invalid2@users.noreply.github.com>
2020-12-04 18:21:23 -0300

    Tried fixing the iframe bug again
    title

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ var activatorbtn = null;
 function onReleasebtnClick() {
 	bottomdiv[0].style.display = bottomdiv[0].style.display;
 	if(bottomdiv[0].style.display === "block" && activatorbtn == 0) {
-		iframe[0].src = "none";
+		iframe[0].src = "emptyframe.html";
 		bottomdiv[0].style.display = "none";
 		imagediv[0].style.filter = "none";
 	} else {
@@ -20,7 +20,7 @@ function onReleasebtnClick() {
 function onGuidesbtnClick() {
 	bottomdiv[0].style.display = bottomdiv[0].style.display;
 	if(bottomdiv[0].style.display === "block" && activatorbtn == 1) {
-		iframe[0].src = "none";
+		iframe[0].src = "emptyframe.html";
 		bottomdiv[0].style.display = "none";
 		imagediv[0].style.filter = "none";
 	} else {
