c6701293aa13f57c39903c071c3c68ebe6d0eb6a
invalid2 <75389191+invalid2@users.noreply.github.com>
2020-12-04 17:49:56 -0300

    Inital website version
    title

---
+++ script.js
@@ -0,0 +1,30 @@
+var bottomdiv = document.getElementsByClassName("downdiv");
+var iframe = document.getElementsByClassName("mainframe");
+var imagediv = document.getElementsByClassName("imagediv");
+var activatorbtn = null;
+
+function onReleasebtnClick() {
+	bottomdiv[0].style.display = bottomdiv[0].style.display;
+	if(bottomdiv[0].style.display === "block" && activatorbtn == 0) {
+		bottomdiv[0].style.display = "none";
+		imagediv[0].style.filter = "none";
+	} else {
+		activatorbtn = 0;
+		iframe[0].src = "releasesframe.html";
+		bottomdiv[0].style.display = "block";
+		imagediv[0].style.filter = "blur(4px)";
+	}
+}
+
+function onGuidesbtnClick() {
+	bottomdiv[0].style.display = bottomdiv[0].style.display;
+	if(bottomdiv[0].style.display === "block" && activatorbtn == 1) {
+		bottomdiv[0].style.display = "none";
+		imagediv[0].style.filter = "none";
+	} else {
+		activatorbtn = 1;
+		iframe[0].src = "guidesframe.html";
+		bottomdiv[0].style.display = "block";
+		imagediv[0].style.filter = "blur(4px)";
+	}
+}
