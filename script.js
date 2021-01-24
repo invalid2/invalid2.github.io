36218bdb422d5b40ec1381c33ff04c6d190b2156
invalid1 <weirdestnerd@gmail.com>
2021-01-23 21:53:22 -0300

    Adde more and changed guides to wiki

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ var iframe = document.getElementsByClassName("mainframe");
 var imagediv = document.getElementsByClassName("imagediv");
 var activatorbtn = null;
 
-function onReleasebtnClick() {
+function onReleasesbtnClick() {
 	bottomdiv[0].style.display = bottomdiv[0].style.display;
 	if(bottomdiv[0].style.display === "block" && activatorbtn == 0) {
 		iframe[0].src = "emptyframe.html";
@@ -17,7 +17,7 @@ function onReleasebtnClick() {
 	}
 }
 
-function onGuidesbtnClick() {
+function onWikibtnClick() {
 	bottomdiv[0].style.display = bottomdiv[0].style.display;
 	if(bottomdiv[0].style.display === "block" && activatorbtn == 1) {
 		iframe[0].src = "emptyframe.html";
@@ -26,6 +26,20 @@ function onGuidesbtnClick() {
 	} else {
 		activatorbtn = 1;
 		iframe[0].src = "guidesframe.html";
+		bottomdiv[0].style.display = "block";
+		imagediv[0].style.filter = "blur(4px)";
+	}
+}
+
+function onMorebtnClick() {
+	bottomdiv[0].style.display = bottomdiv[0].style.display;
+	if(bottomdiv[0].style.display === "block" && activatorbtn == 1) {
+		iframe[0].src = "emptyframe.html";
+		bottomdiv[0].style.display = "none";
+		imagediv[0].style.filter = "none";
+	} else {
+		activatorbtn = 1;
+		iframe[0].src = "moreframe.html";
 		bottomdiv[0].style.display = "block";
 		imagediv[0].style.filter = "blur(4px)";
 	}
