b22c527635eeba16cb24a02ebab2d27556e4a977
invalid1 <weirdestnerd@gmail.com>
2021-01-30 20:45:45 -0300

    fixed typo on moreframe

--- script.js
+++ script.js
@@ -3,6 +3,13 @@ var iframe = document.getElementsByClassName("mainframe");
 var imagediv = document.getElementsByClassName("imagediv");
 var activatorbtn = null;
 
+
+window.addEventListener("load", onImgLoad);
+
+function onImgLoad() {
+	console.log("bruh");
+}
+
 function onReleasesbtnClick() {
 	bottomdiv[0].style.display = bottomdiv[0].style.display;
 	if(bottomdiv[0].style.display === "block" && activatorbtn == 0) {
@@ -12,20 +19,6 @@ function onReleasesbtnClick() {
 	} else {
 		activatorbtn = 0;
 		iframe[0].src = "releasesframe.html";
-		bottomdiv[0].style.display = "block";
-		imagediv[0].style.filter = "blur(4px)";
-	}
-}
-
-function onWikibtnClick() {
-	bottomdiv[0].style.display = bottomdiv[0].style.display;
-	if(bottomdiv[0].style.display === "block" && activatorbtn == 1) {
-		iframe[0].src = "emptyframe.html";
-		bottomdiv[0].style.display = "none";
-		imagediv[0].style.filter = "none";
-	} else {
-		activatorbtn = 1;
-		iframe[0].src = "guidesframe.html";
 		bottomdiv[0].style.display = "block";
 		imagediv[0].style.filter = "blur(4px)";
 	}
